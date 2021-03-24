b9ba56dbd2f12fb21bcfd70a6e8d827968eda1b9
GraceYu <librayuyueran@gmail.com>
2021-03-23 22:15:00 -0500

    test of keyword  in Component Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+)
 
 function App() {
   return (
       <div className="App">
-        <HomePage/>
+        <Route path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+
       </div>
   );
 }
