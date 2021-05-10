3f782f173921cce8ee197a5975a8bd28938ea5a2
GraceYu <librayuyueran@gmail.com>
2021-05-09 22:38:26 -0500

    put the constant string SET_CURRENT_USER into a js file incase keep everywhere consistent

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,9 @@
-
 // user is a parameter, you can pass a object parameter here
 // this shape of an object is all a action is
-export const setCurrentUser = user =>({
-  type: 'SET_CURRENT_USER',
+
+import {userActionTypes} from ' ./user.types'
+
+export const setCurrentUser = user => ({
+  type: userActionTypes.SET_CURRENT_USER,
   payload: user
 })
