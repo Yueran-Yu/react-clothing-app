a1bd206cfb7725adea37951a551ed7c7756f7a63
GraceYu <librayuyueran@gmail.com>
2021-05-27 22:43:12 -0500

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,19 +6,26 @@ import {connect} from 'react-redux';
 import {selectCartItems} from '../../redux/cart/cart.selectors'
 import {createStructuredSelector} from "reselect";
 import {withRouter} from 'react-router-dom';
+import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
-const CartDropDown = ({cartItems, history}) => (
-    <div className='cart-dropdown'>
-      <div className='cart-items'>
-        {
-          cartItems.length ?
-              cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
-              : (<div className='empty-message'>Your cart is empty</div>)
-        }
-      </div>
-      <CustomerButton onClick={()=> history.push('/checkout')}>GO TO CHECKOUT</CustomerButton>
-    </div>
-)
+
+const CartDropDown = ({cartItems, history, dispatch}) => {
+  console.log(dispatch)
+  return (
+      <div className='cart-dropdown'>
+        <div className='cart-items'>
+          {
+            cartItems.length ?
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+                : (<div className='empty-message'>Your cart is empty</div>)
+          }
+        </div>
+        <CustomerButton onClick={() => {
+          history.push('/checkout')
+          dispatch(toggleCartHidden())
+        }}> GO TO CHECKOUT </CustomerButton>
+      </div>)
+}
 
 // by using select we do not need to re-render
 // which helps save us on performance
