89f7a2365548b0bb60dd83a801570120b08204a0
GraceYu <librayuyueran@gmail.com>
2021-05-30 21:12:13 -0500

    update imformation

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import {Route} from 'react-router-dom';
+import CategoryPage from "../category/category.component";
 
-const ShopPage = () => (
-    <div className='shop-page'>
-      <CollectionsOverview/>
-    </div>
-)
+const ShopPage = ({match}) => {
+  console.log(match)
+  return (
+      <div className='shop-page'>
+        {/* /shop/:category */}
+        < Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        {/* This makes it more flexible if we want to reuse it in another place! */}
+        < Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
+      </div>
+  )
+}
 
 export default ShopPage;
 // ShopPage is becoming a simple non-connected component now.
