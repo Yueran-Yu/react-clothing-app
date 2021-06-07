82a960bafc892b951f5a3f3aaba0a0538d0cd9f2
GraceYu <librayuyueran@gmail.com>
2021-06-06 23:49:44 -0500

    add react-stripe-checkout to the app

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IzZCdDoG3CMesjQ7WomYduwEK2eiGLEUVb5La2zJcDVccXUGgAMjXrs8RL9xVVXQnlSzHPyc8aQmk5y6sGgG9Jy003RDq1ORY';
+onToken = token =>{
+  console.log(token);
+  alert('Payment Successful')
+}
+  return (
+      <StripeCheckout
+          label="Pay Now"
+          name='CRWN Clothing Ltd.'
+          billingAddress
+          shippingAddress
+          image='https://svgshare.com/i/CUz.svg'
+          description={`Your total is $${price}`}
+          amount={priceForStripe}
+          panelLabel='Pay Now'
+      />
+  )
+}
