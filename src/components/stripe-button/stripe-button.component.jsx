effc718b9c71084e3c84406a48a8b4ecc330b23d
GraceYu <librayuyueran@gmail.com>
2021-06-07 00:35:39 -0500

    complete the stripe

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,10 +4,11 @@ import StripeCheckout from 'react-stripe-checkout';
 const StripeCheckoutButton = ({price}) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51IzZCdDoG3CMesjQ7WomYduwEK2eiGLEUVb5La2zJcDVccXUGgAMjXrs8RL9xVVXQnlSzHPyc8aQmk5y6sGgG9Jy003RDq1ORY';
-onToken = token =>{
-  console.log(token);
-  alert('Payment Successful')
-}
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful')
+  }
   return (
       <StripeCheckout
           label="Pay Now"
@@ -18,6 +19,10 @@ onToken = token =>{
           description={`Your total is $${price}`}
           amount={priceForStripe}
           panelLabel='Pay Now'
+          token={onToken}
+          stripeKey={publishableKey}
       />
   )
 }
+
+export default StripeCheckoutButton;
